44bc85164923d0546c8921c5521ae423853a6fd0
John Wood <john.wood@centro.net>
2014-01-09 13:14:21 -0600

    Fix indentation

--- lib/application_seeds/capistrano.rb
+++ lib/application_seeds/capistrano.rb
@@ -1,7 +1,7 @@
 require 'capistrano'
 
 if defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
-    load File.expand_path('capistrano/tasks/application_seeds.rake', File.dirname(__FILE__))
+  load File.expand_path('capistrano/tasks/application_seeds.rake', File.dirname(__FILE__))
 else
-    require_relative 'capistrano2'
+  require_relative 'capistrano2'
 end
