cda1ec43f7885bd63c5364ff7b0637f19c59db6b
Jeremy D. Frens <jdfrens@gmail.com>
2014-05-07 16:52:37 -0500

    Use indifferent access for key remapping.
    And use an inject.

--- lib/application_seeds/attributes.rb
+++ lib/application_seeds/attributes.rb
@@ -9,24 +9,19 @@ module ApplicationSeeds
 
     def select_attributes(*attribute_names)
       attribute_names.map!(&:to_s)
-      Attributes.new(select { |k,v| attribute_names.include?(k) })
+      Attributes.new(select { |k, v| attribute_names.include?(k) })
     end
 
     def reject_attributes(*attribute_names)
       attribute_names.map!(&:to_s)
-      Attributes.new(reject { |k,v| attribute_names.include?(k) })
+      Attributes.new(reject { |k, v| attribute_names.include?(k) })
     end
 
     def map_attributes(mapping)
-      mapping.stringify_keys!
-
-      mapped = {}
-      each do |k,v|
-        if mapping.keys.include?(k)
-          mapped[mapping[k].to_s] = v
-        else
-          mapped[k] = v
-        end
+      mapping = mapping.with_indifferent_access
+      mapped  = inject({}) do |hash, (k, v)|
+        mapped_key = mapping.fetch(k) { k }
+        hash.merge!(mapped_key => v)
       end
       Attributes.new(mapped)
     end
